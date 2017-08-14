1ab06910cab3697c46c1658353ee1a4b695fef2f
Mina Hyeon <hellostranger.mm@gmail.com>
2017-08-15 02:05:34 +0900

    moving-the-player
    Input.GetAxis and Rigidbody.AddForce

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    
-	void FixedUpdate ()
+
+    private Rigidbody rb;
+
+    private void Start()
     {
-        Input
-	}
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement);
+    } 
 }
